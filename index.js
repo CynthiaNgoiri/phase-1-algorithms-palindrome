2907499e4bd8cf539a116d5459e547248163174f
Cynthia Njomo <cynthiangoirinjomo@gmail.com>
2022-08-26 14:25:53 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-function isPalindrome(word) {
+// function isPalindrome(word) {
   // Write your algorithm here
-}
+// }
 
 /* 
   Add your pseudocode here
-*/
+// */ 
+// use conditional statement if
+// compare with strict equality operator
+// return true or false depending on argument passed
+
 
 /*
   Add written explanation of your solution here
 */
+const isPalindrome=(str)=>{
+  let reversed=str.split("").reverse().join("");
+
+  if (reversed=== str)return true;
+
+  return false;
+
+}
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
